93bea200e5b5cad2dbfc676dce3223ef31550786
pawankumar45 <122915635+pawankumar45@users.noreply.github.com>
2024-08-01 19:40:27 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -43,8 +43,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>
 );
